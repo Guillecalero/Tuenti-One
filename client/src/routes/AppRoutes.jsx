d27f36968a97902eb33e09df1fddff89fc10f16f
Andrés García <playsharp59@gmail.com>
2022-03-01 16:58:21 +0100

    added navbar logout

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -2,13 +2,16 @@ import { Route, Routes } from 'react-router-dom'
 import RegisterPage from '../pages/RegisterPage/RegisterPage'
 import LoginPage from '../pages/LoginPage/LoginPage'
 import ProfilePage from '../pages/ProfilePage/ProfilePage'
+import HomePage from '../pages/HomePage/HomePage'
 
 const AppRoutes = () => {
     return (
         <Routes>
 
+            <Route path='/home' element={<HomePage />} />
             <Route path='/registro' element={<RegisterPage />} />
-            <Route path='/login' element={<LoginPage />} />
+            {/* Login page as the principal page */}
+            <Route path='/' element={<LoginPage />} />
             <Route path='/profile' element={<ProfilePage />} />
 
         </Routes>
