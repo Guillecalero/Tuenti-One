00c897025a719971b925daf909e6e2928efdcd57
Andrés García <playsharp59@gmail.com>
2022-03-02 10:50:00 +0100

    fixed conflicts

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -3,10 +3,7 @@ import RegisterPage from '../pages/RegisterPage/RegisterPage'
 import LoginPage from '../pages/LoginPage/LoginPage'
 import ProfilePage from '../pages/ProfilePage/ProfilePage'
 import HomePage from '../pages/HomePage/HomePage'
-<<<<<<< HEAD
 import ProfilePageEdit from '../pages/ProfilePageEdit/ProfilePageEdit'
-=======
->>>>>>> d27f369 (added navbar logout)
 
 const AppRoutes = () => {
     return (
@@ -16,12 +13,8 @@ const AppRoutes = () => {
             <Route path='/registro' element={<RegisterPage />} />
             {/* Login page as the principal page */}
             <Route path='/' element={<LoginPage />} />
-<<<<<<< HEAD
             <Route path='/perfil' element={<ProfilePage />} />
             <Route path='/editar' element={<ProfilePageEdit />} />
-=======
-            <Route path='/profile' element={<ProfilePage />} />
->>>>>>> d27f369 (added navbar logout)
 
         </Routes>
     )
